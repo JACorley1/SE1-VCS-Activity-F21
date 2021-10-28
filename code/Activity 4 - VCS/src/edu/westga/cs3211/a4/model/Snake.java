1d2a14de40d924a5d537afbb943914613f435417
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2021-10-28 13:46:01 -0400

    added documentation to the class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
@@ -1,17 +1,19 @@
 package edu.westga.cs3211.a4.model;
 
+/**
+ * Creates an object of Class Snake.
+ */
 public class Snake {
 	private double length;
 	private boolean venomous;
 	
-	public Snake(double length, boolean venomous) {
-		this.length = length;
-		this.venomous = venomous ;
-	}
-
-	private double length;
-	private boolean venomous;
 	
+	/**
+	 * Instantiates a new snake.
+	 *
+	 * @param length the length
+	 * @param isVenomous the is venomous
+	 */
 	public Snake(double length, boolean isVenomous) {
 		if (length <= 0) {
 			throw new IllegalArgumentException("Snake length must be greater than zero");
