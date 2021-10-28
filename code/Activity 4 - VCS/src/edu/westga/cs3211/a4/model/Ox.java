856fffaf408bce31000461ea337163d9a50446f8
Tyler Coltrain <tcoltra1@my.westga.edu>
2021-10-28 13:48:15 -0400

    Java doc for Ox class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Ox.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Ox.java
@@ -1,16 +1,44 @@
 package edu.westga.cs3211.a4.model;
 
+/**
+ * Ox class
+ * @author Tyler Coltrain
+ *
+ */
 public class Ox {
-int oxYear;
-String oxType;
+private int oxYear;
+private String oxType;
 
+/**
+ * creates an Ox with the given year and type
+ * @param year	year of the ox
+ * @param type	type of the ox
+ */
 	public Ox(int year, String type) {
 		this.oxYear = year;
 		this.oxType = type;
 	}
-
+/**
+ * default constructor of an Ox
+ */
 	public Ox() {
 		this.oxYear = 1997;
 		this.oxType = "Fire";
+	}
+	
+	/**
+	 * gets the ox year
+	 * @return year of the ox
+	 */
+	public int getOxYear() {
+		return this.oxYear;
+	}
+	
+	/**
+	 * gets the ox type
+	 * @return type of the ox
+	 */
+	public String getOxString() {
+		return this.oxType;
 	}
 }
