5d476041226abb03e0c354f2b39cdcfcb04955a5
Tyler Jones <76069841+wjones17@users.noreply.github.com>
2021-10-28 13:35:09 -0400

    added two fields and constructor

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
@@ -1,5 +1,12 @@
 package edu.westga.cs3211.a4.model;
 
 public class Snake {
+	private double length;
+	private boolean venomous;
+	
+	public Snake(double length, boolean venomous) {
+		this.length = length;
+		this.venomous = venomous ;
+	}
 
 }
