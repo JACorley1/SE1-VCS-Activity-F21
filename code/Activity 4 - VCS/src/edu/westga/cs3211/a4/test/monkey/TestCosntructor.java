96f21909402850f30188fb0cff98b7c94eb9d273
Matthew Thompson <mthomp35@my.westga.edu>
2021-10-28 13:49:17 -0400

    Created the tests for the constructor

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/monkey/TestCosntructor.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/monkey/TestCosntructor.java
@@ -7,8 +7,39 @@ import org.junit.jupiter.api.Test;
 class TestCosntructor {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNullHasTail() {
+		//ASSERT
+		assertThrows(IllegalArgumentException.class,
+				()->{
+					Monkey monkey = new Monkey(null, "Curious George");
+				}
+				);
+	}
+	
+	@Test
+	void testNullName() {
+		//ASSERT
+		assertThrows(IllegalArgumentException.class,
+				()->{
+					Monkey monkey = new Monkey(true, null);
+				}
+				);
+	}
+	@Test
+	void testEmptyName() {
+		//ASSERT
+		assertThrows(IllegalArgumentException.class,
+				()->{
+					Monkey monkey = new Monkey(true, "");
+				}
+				);
+	}
+	
+	@Test
+	void testValidMonkey() {
+		Monkey monkey = new Monkey(true, "Curious George";
+		assertEquals(monkey.hasTail, true, "Checks if monkey has tail is correct");
+		assertEquals(monkey.getName, "Curious George", "Checks if species matches");
 	}
 
 }
