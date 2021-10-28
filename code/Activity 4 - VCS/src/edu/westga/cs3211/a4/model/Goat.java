ac0bcffca5fb060da9bf781f5fd65fb43b2d969a
Miguel Campos <csuser@cs.westga.edu>
2021-10-28 13:48:00 -0400

    Constructor

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Goat.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Goat.java
@@ -3,4 +3,9 @@ package edu.westga.cs3211.a4.model;
 public class Goat {
 	private int age;
 	private boolean isAlive;
+	
+	public Goat(int goatAge, boolean isAlive) {
+		this.age = goatAge;
+		this.isAlive = isAlive;
+	}
 }
