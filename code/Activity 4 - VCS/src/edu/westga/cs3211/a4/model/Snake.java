10c9b7a3a795401499abf4305b2e72aadb9efb0a
csuser@cs.westga.edu <csuser@cs.westga.edu>
2021-10-28 13:30:58 -0400

    added fields, constructor

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Snake.java
@@ -2,4 +2,16 @@ package edu.westga.cs3211.a4.model;
 
 public class Snake {
 
+	private double length;
+	private boolean venomous;
+	
+	public Snake(double length, boolean isVenomous) {
+		if (length <= 0) {
+			throw new IllegalArgumentException("Snake length must be greater than zero");
+		}
+		this.length = length;
+		this.venomous = isVenomous;
+	}
+	
+	public 
 }
