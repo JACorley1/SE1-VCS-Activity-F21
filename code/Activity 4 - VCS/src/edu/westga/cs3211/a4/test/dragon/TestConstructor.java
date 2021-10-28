b896e9255e9e658a77c9159d6c9e0d4bf5c9ace9
Oumar Diallo <odiallo1@my.westga.edu>
2021-10-28 13:49:14 -0400

    added valid test constructor test

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/dragon/TestConstructor.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/dragon/TestConstructor.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs3211.a4.model.Dragon;
+
 class TestConstructor {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testValidConstructor() {
+		Dragon toothLess = new Dragon("red", 5);
+		
+		assertAll(()-> assertEquals("red", toothLess.getColor()),
+				  ()-> assertEquals(5, toothLess.getWingspan())
+				);
 	}
 
 }
