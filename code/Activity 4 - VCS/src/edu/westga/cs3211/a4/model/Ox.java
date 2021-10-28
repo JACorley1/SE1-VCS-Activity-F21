3d46577d2dabf7b78f58891cffd5d17b6c2335ba
csuser@cs.westga.edu <csuser@cs.westga.edu>
2021-10-28 13:36:26 -0400

    created Ox class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Ox.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Ox.java
@@ -1,5 +1,16 @@
 package edu.westga.cs3211.a4.model;
 
 public class Ox {
+int oxYear;
+String oxType;
 
+	public Ox(int year, String type) {
+		this.oxYear = year;
+		this.oxType = type;
+	}
+
+	public Ox() {
+		this.oxYear = 1997;
+		this.oxType = "Fire";
+	}
 }
