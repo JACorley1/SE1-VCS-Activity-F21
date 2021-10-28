4d0afe2899219ddaa463e8c9908c418b7d7b0b29
Bryson Frederick <bfreder1@my.westga.edu>
2021-10-28 13:52:54 -0400

    Javadoc edits Constructor
    Partially implemented constructor
    Partially wrote javadocs

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Tiger.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Tiger.java
@@ -1,8 +1,22 @@
 package edu.westga.cs3211.a4.model;
 
+/**
+* The Tiger Class.
+*
+* @author Bryson Frederick
+*/
+
 public class Tiger {
 	
 	private boolean hasStripes;
 	private String color;
+	
+	//todo: Add javadoc and finish preconditions.
+	
+	public Tiger(boolean hasStripes, String color){
+		if (color == null || 
+		this.hasStripes = hasStripes;
+		this.color = color;
+	}
 
 }
