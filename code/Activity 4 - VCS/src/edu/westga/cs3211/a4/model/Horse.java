7c58f53b25195dde04576269130028f33d9b36d4
JaneraSmith <jsmit189@my.westga.edu>
2021-10-29 10:51:49 -0400

    Feature-Horse: Added a 2-parameter constructor to the Horse class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Horse.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Horse.java
@@ -9,5 +9,26 @@ package edu.westga.cs3211.a4.model;
 public class Horse {
 	private String name;
 	private boolean isVicious;
-	
+
+	/**
+	 * This will set up the Horse object with the specified name of the Horse and
+	 * whether the Horse is vicious
+	 * 
+	 * @precondition name != null && !name.isEmpty()
+	 * @postcondition getName() == name && isVicious() == isVicious
+	 * 
+	 * @param name      the name of the Horse
+	 * @param isVicious true if the Horse is vicious; false otherwise
+	 */
+	public Horse(String name, boolean isVicious) {
+		if (name == null) {
+			throw new IllegalArgumentException("The Horse's name cannot be null");
+		}
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("The Horse's name cannot be empty");
+		}
+
+		this.name = name;
+		this.isVicious = isVicious;
+	}
 }
