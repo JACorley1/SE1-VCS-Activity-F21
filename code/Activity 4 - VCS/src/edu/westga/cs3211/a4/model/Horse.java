8efe51b36aad2967f4dd98df9fdcdc6e8bb12af3
JaneraSmith <jsmit189@my.westga.edu>
2021-10-29 10:40:32 -0400

    Feature-Horse: Added private fields to the Horse class

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Horse.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/model/Horse.java
@@ -1,5 +1,13 @@
 package edu.westga.cs3211.a4.model;
 
+/**
+ * This Horse class will set up the name of the Horse and whether it is vicious
+ * 
+ * @author Janera Smith
+ * @version Fall 2021
+ */
 public class Horse {
-
+	private String name;
+	private boolean isVicious;
+	
 }
