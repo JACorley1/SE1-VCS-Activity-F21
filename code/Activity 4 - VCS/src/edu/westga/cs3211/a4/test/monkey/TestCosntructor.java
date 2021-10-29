97c3d95848597d81d3c2637f7c50c66954cc6a6a
Steven Carriger <scarrig1@my.westga.edu>
2021-10-29 12:47:29 -0400

    renamed tests to be easier to understand, simplified the tests, imported the model class.

--- code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/monkey/TestCosntructor.java
+++ code/Activity 4 - VCS/src/edu/westga/cs3211/a4/test/monkey/TestCosntructor.java
@@ -4,42 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestCosntructor {
+import edu.westga.cs3211.a4.model.Monkey;
 
+class TestCosntructor {
 	@Test
-	void testNullHasTail() {
-		//ASSERT
+	void testWhenNameIsNull() {
 		assertThrows(IllegalArgumentException.class,
 				()->{
-					Monkey monkey = new Monkey(null, "Curious George");
-				}
-				);
-	}
-	
-	@Test
-	void testNullName() {
-		//ASSERT
-		assertThrows(IllegalArgumentException.class,
-				()->{
-					Monkey monkey = new Monkey(true, null);
+					new Monkey(true, null);
 				}
 				);
 	}
 	@Test
-	void testEmptyName() {
-		//ASSERT
+	void testWhenNameIsEmpty() {
 		assertThrows(IllegalArgumentException.class,
 				()->{
-					Monkey monkey = new Monkey(true, "");
+					new Monkey(true, "");
 				}
 				);
 	}
 	
 	@Test
 	void testValidMonkey() {
-		Monkey monkey = new Monkey(true, "Curious George";
-		assertEquals(monkey.hasTail, true, "Checks if monkey has tail is correct");
-		assertEquals(monkey.getName, "Curious George", "Checks if species matches");
+		Monkey monkey = new Monkey(true, "Curious George");
+		assertEquals(monkey.isHasTail(), true, "Checks if monkey has tail is correct");
+		assertEquals(monkey.getName(), "Curious George", "Checks if name matches");
 	}
 
 }
